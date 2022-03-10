d71878a9c20a7007eec42f04a658dcc2240e4e13
Dmitriy U <akiba@wolfness.org>
2022-03-10 13:58:15 +0500

    Fixed path to php.ini in docker entrypoint scripts.

--- 7.1-fpm/docker-entrypoint.sh
+++ 7.1-fpm/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i -e "s/\$ENV_PHP_POST_MAX_SIZE/${PHP_POST_MAX_SIZE}/g" /usr/local/etc/php.ini
-sed -i -e "s/\$ENV_PHP_UPLOAD_MAX_FILESIZE/${PHP_UPLOAD_MAX_FILESIZE}/g" /usr/local/etc/php.ini
+sed -i -e "s/\$ENV_PHP_POST_MAX_SIZE/${PHP_POST_MAX_SIZE}/g" /usr/local/etc/php/php.ini
+sed -i -e "s/\$ENV_PHP_UPLOAD_MAX_FILESIZE/${PHP_UPLOAD_MAX_FILESIZE}/g" /usr/local/etc/php/php.ini
 
 sed -i -e "s/\$ENV_FPM_USER/${FPM_USER}/g" /usr/local/etc/php-fpm.d/www.conf
 sed -i -e "s/\$ENV_FPM_GROUP/${FPM_GROUP}/g" /usr/local/etc/php-fpm.d/www.conf
